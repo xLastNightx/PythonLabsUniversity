b4432a2fc65eea5beb8a97a163ed07167a5024c4
Kate <109430305+xLastNightx@users.noreply.github.com>
2025-09-30 15:21:13 +0300

    Create lab3.py
    a few

---
+++ lab3.py
@@ -0,0 +1,13 @@
+class Bank:
+    def openBankAccount():
+    pass
+
+yourBank = Bank()
+
+class BankAccount:
+    def openBankAccount():
+
+class Client:
+    def signUp():
+    def signOut():
+    def openBankAccount():
